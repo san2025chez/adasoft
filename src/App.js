cb36fcca3b3057793e7aff6fba0d00ffb27d1e33
ana <vane2013sanchez@gmail.com>
2023-12-18 10:44:56 -0500

    add-image-response

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import {Formulario} from './components/Formulario'
+import { Formulario } from './components/Formulario'
 import { NavBar } from "./components/NavBar";
 import './App.css';
+import styled from '@emotion/styled';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main:'#FFFFFF', // Azul claro
+      main: '#FFFFFF', // Azul claro
     },
     secondary: {
       main: '#1976d2', // Azul más oscuro para acentos
@@ -32,156 +33,209 @@ const App = () => {
     }
   };
 
+  const GridItem = styled.div`
+    background-image: url("${process.env.PUBLIC_URL}/images/metodologia.jpg");
+    background-size: cover;
+    background-position: center;
+    height: 450px;
+  
+    margin-top: 40px;
+    width: 50%;
+    @media (max-width: 600px) {
+      height: 200px;
+      width: 270px;
+      marginTop: 50px
+    }
+  `;
+
+  const GridItemNosotros = styled.div`
+   
+    background-size: cover;
+    background-position: center;
+    margin-top: 40px;
+    height: 350px;
+    width: 50%;
+ 
+    background-image: url('${process.env.PUBLIC_URL}/images/nosotros.jpg');
+        @media (max-width: 600px) {
+          height: 200px;
+          width: 270px;
+     
+      
+      
+    }
+  `;
+  const TextComponent = styled.div`
+ fontSize:20px,
+ marginTop: 40px
+ paddingTop: 40px
+`;
+
+  const GridPaper = styled(Paper)`
+    padding: 0px;
+    margin: 0px;
+    box-shadow: none;
+    background-image: url(${process.env.PUBLIC_URL}/images/miportada.jpg);
+    background-size: cover;
+    background-position: center;
+    height: 500px;
+    width: 100%
+    position: relative;
+    @media (max-width: 600px) {
+ 
+      background-image: url(${process.env.PUBLIC_URL}/images/metodologia.jpg);
+      background-size: contain;
+      background-repeat: no-repeat;
+      height: 300px
+
+  
+  
+}
+  `;
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="xl" style={{ padding: 0, margin: 0, minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <NavBar/>
-        <Paper id="inicio" elevation={3} style={{ padding: '0px', marginTop: '0px',boxShadow: 'none'  }}>
+        <NavBar />
+       
+        <GridPaper
 
-          <img
-            src="images/miportada.jpg"
-            alt="Descripción de la imagen"
-            style={{ maxWidth: '100%',height:'70%' }}
-            className="imagen-inicio"
-          />
-          <Typography style={{padding:'20px'}}>
-          En ADA soft presentamos un modelo innovador de consultoría tecnológica.
-Tenemos una mirada global: nos especializamos en desarrollo de soluciones
-que agregan valor a todas las áreas de las organizaciones. 
+        >
+       
+        </GridPaper>
+        <Container style={{ marginTop:'30px'}}>
+        <TextComponent>
+                      En Ada Software agregamos valor al área IT de las organizaciones a través de servicios administrados y personalizados. Contamos con un equipo experto a disposición de nuestros clientes, para que puedan aprovechar al máximo nuestra colaboración.
+        
+        </TextComponent>
+        </Container>
 
-          </Typography>
-          
-        </Paper>
-        <Paper id="servicios" elevation={3} style={{ padding: '20px', margin: '20px 10px', textAlign: 'center',boxShadow: 'none' }}>
-        <Container maxWidth="md" style={{ padding: '20px' }}>
-          <Typography variant="h4" color="primary" style={{ fontWeight: 'bold' ,color: '#1976d2'}}>Servicios</Typography>
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item xs={12} md={4}>
-              <Paper style={{ padding: '20px' }}>
-                <Typography variant="h5"> Managed Services</Typography>
-                <Typography >
-                
+        <Paper id="servicios" elevation={3} style={{ padding: '20px', margin: '20px 10px', textAlign: 'center', boxShadow: 'none' }}>
+          <Container maxWidth="md" style={{ padding: '20px' }}>
+            <Typography variant="h4" color="primary" style={{ fontWeight: 'bold', color: '#1976d2' }}>Servicios</Typography>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={12} md={4}>
+                <Paper style={{ padding: '20px' }}>
+                  <Typography variant="h5"> Managed Services</Typography>
+                  <Typography >
 
-                  En Ada Software agregamos valor al área IT de las organizaciones a través de servicios administrados y personalizados. Contamos con un equipo experto a disposición de nuestros clientes, para que puedan aprovechar al máximo nuestra colaboración.
-                </Typography>
-            
-              </Paper>
+
+                    En Ada Software agregamos valor al área IT de las organizaciones a través de servicios administrados y personalizados. Contamos con un equipo experto a disposición de nuestros clientes, para que puedan aprovechar al máximo nuestra colaboración.
+                  </Typography>
+
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Paper style={{ padding: '20px', boxShadow: 'none' }}>
+                  <Typography variant="h5" > IT Consulting</Typography>
+                  <Typography>
+
+
+                    Ayudamos a definir y alinear la estrategia de IT con la de la organización. Contamos con las habilidades y la experiencia adecuadas, brindamos herramientas que posibilitan mejorar y optimizar procesos, ahorrando tiempo y dinero, simplificando proyectos complejos y protegiendo los datos comerciales de nuestros clientes. En Ada Software transformamos negocios.
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Paper style={{ padding: '20px', boxShadow: 'none' }}>
+                  <Typography variant="h5">Soluciones </Typography>
+                  <Typography>Brindamos soluciones a medida de cada empresa, sin importar su tamaño. Les damos a nuestros clientes las herramientas para comprender y utilizar la tecnología en favor de sus proyectos. </Typography>
+                </Paper>
+
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={4}>
-              <Paper style={{ padding: '20px', boxShadow: 'none'  }}>
-                <Typography variant="h5" > IT Consulting</Typography>
-                <Typography>
-                 
-
-                  Ayudamos a definir y alinear la estrategia de IT con la de la organización. Contamos con las habilidades y la experiencia adecuadas, brindamos herramientas que posibilitan mejorar y optimizar procesos, ahorrando tiempo y dinero, simplificando proyectos complejos y protegiendo los datos comerciales de nuestros clientes. En Ada Software transformamos negocios.
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Paper style={{ padding: '20px',boxShadow: 'none'  }}>
-                <Typography variant="h5">Soluciones </Typography>
-                <Typography>Brindamos soluciones a medida de cada empresa, sin importar su tamaño. Les damos a nuestros clientes las herramientas para comprender y utilizar la tecnología en favor de sus proyectos. </Typography>
-              </Paper>import { NavBar } from './components/NavBar';
-
-            </Grid>
-          </Grid>
           </Container>
         </Paper>
-        <Paper id="metodologia" elevation={3} style={{ padding: '30px', marginTop: '30px', marginLeft: '10px', marginRight: '10px', textAlign: 'center',boxShadow: 'none' }}>
-          <Typography variant="h4" color="primary" style={{ fontWeight: 'bold',color: '#1976d2' }}>Nuestra Metodología</Typography>
+        <Paper id="metodologia" elevation={3} style={{ padding: '30px', marginTop: '30px', marginLeft: '10px', marginRight: '10px', textAlign: 'center', boxShadow: 'none' }}>
+          <Typography variant="h4" color="primary" style={{ fontWeight: 'bold', color: '#1976d2' }}>Nuestra Metodología</Typography>
           {/* Cuadrícula para dividir en dos partes */}
           <Container maxWidth="md" style={{ padding: '5px' }}>
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item xs={12} md={6}>
-              {/* Contenido de la parte izquierda (imagen) */}
-              <img
-                src="images/metodologia.jpg"
-                alt="Imagen de la metodología"
-                style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
-              />
+            <Grid container spacing={2} justifyContent="center">
+              <GridItem></GridItem>
+              <Grid item xs={12} md={6}>
+                {/* Contenido de la parte derecha (descripción) */}
+                <Paper
+                  id="metodologia"
+                  style={{ padding: '5px',boxShadow: 'none', marginTop:'20px' }}
+                >
+                  <Typography component="div">
+                    <Typography variant="h5" gutterBottom style={{ textAlign: 'left', }}>
+                      Fase 1: Consultoría
+                    </Typography>
+                    <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
+                      <li>Analizamos los procesos.</li>
+                      <li>Detectamos oportunidades.</li>
+                      <li>Evaluamos en qué parte del proceso agregar valor.</li>
+                    </ul>
+
+                    <Typography variant="h5" gutterBottom style={{ textAlign: 'left', marginLeft: '20px' }}>
+                      Fase 2: Propuesta
+                    </Typography>
+                    <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
+                      <li>Ofrecemos soluciones, presentando nuestra propuesta de valor.</li>
+                      <li>Exponemos los beneficios de implementarlas.</li>
+                      <li>Analizamos el retorno de la inversión.</li>
+                    </ul>
+
+                    <Typography variant="h5" gutterBottom style={{ textAlign: 'left', marginLeft: '20px' }}>
+                      Fase 3: Implementación
+                    </Typography>
+                    <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
+                      <li>Comenzamos el proceso de implementación.</li>
+                      <li>Damos soporte.</li>
+                      <li>Realizamos seguimiento de los cambios.</li>
+                    </ul>
+                  </Typography>
+
+
+
+
+
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              {/* Contenido de la parte derecha (descripción) */}
-              <Paper style={{ padding: '20px', marginTop: '20px' ,boxShadow: 'none' }}>
-
-                <Typography component="div">
-                  <Typography variant="h5" gutterBottom style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    Fase 1: Consultoría
-                  </Typography>
-                  <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    <li>Analizamos los procesos.</li>
-                    <li>Detectamos oportunidades.</li>
-                    <li>Evaluamos en qué parte del proceso agregar valor.</li>
-                  </ul>
-
-                  <Typography variant="h5" gutterBottom style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    Fase 2: Propuesta
-                  </Typography>
-                  <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    <li>Ofrecemos soluciones, presentando nuestra propuesta de valor.</li>
-                    <li>Exponemos los beneficios de implementarlas.</li>
-                    <li>Analizamos el retorno de la inversión.</li>
-                  </ul>
-
-                  <Typography variant="h5" gutterBottom style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    Fase 3: Implementación
-                  </Typography>
-                  <ul style={{ textAlign: 'left', marginLeft: '20px' }}>
-                    <li>Comenzamos el proceso de implementación.</li>
-                    <li>Damos soporte.</li>
-                    <li>Realizamos seguimiento de los cambios.</li>
-                  </ul>
-                </Typography>
-              </Paper>
-            </Grid>
-          </Grid>
           </Container>
         </Paper>
-        <Paper id="nosotros" elevation={3} style={{ padding: '30px', marginTop: '30px',boxShadow: 'none'  }}>
-        <Container maxWidth="md" style={{ padding: '20px' }}>
-          <Typography variant="h4" color="primary" style={{ fontWeight: 'bold',color: '#1976d2', textAlign: 'center'  }}>Nosotros</Typography>
-          {/* Cuadrícula para dividir en dos partes */}
-         
-          <Grid container spacing={2} justifyContent="center">
-
-            <Grid item xs={12} md={6}>
-              {/* Contenido de la parte derecha (descripción) */}
-              <Paper style={{ padding: '20px', marginTop: '20px' }}>
-                <Typography>
-
-
-                  En ADA SOFTWARE no trabajamos de manera estandarizada, sino que adaptamos las mejoras a cada organización, según sus necesidades y sus proyecciones. Nuestras soluciones son a medida de cada cliente.
-
-                  Contamos con años de trayectoria en el mercado que nos permiten analizar, proponer mejoras, resolver problemas y generar cambios superadores desde una mirada experimentada y profesional.
-
-                  Trabajamos codo a codo con cada cliente para resolver, optimizar y generar nuevos proyectos.
-
-                  Somos el aliado que tu equipo de trabajo necesita.
-                  Somos ADA SOFTWARE – Fabricamos soluciones.
-
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              {/* Contenido de la parte izquierda (imagen) */}
-              <img
-                src="images/nosotros.jpg"
-                alt="Imagen de la metodología"
-                style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
-              />
-            </Grid>
-
-          </Grid>
-          </Container>
-        </Paper>
-        <Paper id="contacto" elevation={3} style={{ padding: '20px', marginTop: '20px' ,boxShadow: 'none' }}>
+        <Paper id="nosotros" elevation={3} style={{ padding: '30px', marginTop: '30px', boxShadow: 'none' }}>
           <Container maxWidth="md" style={{ padding: '20px' }}>
-          <Typography variant="h4" color="primary" style={{ fontWeight: 'bold',color: '#1976d2', textAlign: 'center'  }}>Contacto</Typography>
-          {/* Cuadrícula para dividir en dos partes */}
-          <Typography variant="h5" color="primary" style={{ fontWeight: 'bold' }}>Comunicate</Typography>
-       <Formulario></Formulario>
-       </Container>
+            <Typography variant="h4" color="primary" style={{ fontWeight: 'bold', color: '#1976d2', textAlign: 'center' }}>Nosotros</Typography>
+            {/* Cuadrícula para dividir en dos partes */}
+
+            <Grid container spacing={2} justifyContent="center">
+
+              <Grid item xs={12} md={6}>
+                {/* Contenido de la parte derecha (descripción) */}
+                <Paper style={{ padding: '20px', marginTop: '20px',boxShadow: 'none' }}>
+                  <Typography>
+
+
+                    En ADA SOFTWARE no trabajamos de manera estandarizada, sino que adaptamos las mejoras a cada organización, según sus necesidades y sus proyecciones. Nuestras soluciones son a medida de cada cliente.
+
+                    Contamos con años de trayectoria en el mercado que nos permiten analizar, proponer mejoras, resolver problemas y generar cambios superadores desde una mirada experimentada y profesional.
+
+                    Trabajamos codo a codo con cada cliente para resolver, optimizar y generar nuevos proyectos.
+
+                    Somos el aliado que tu equipo de trabajo necesita.
+                    Somos ADA SOFTWARE – Fabricamos soluciones.
+
+                  </Typography>
+                </Paper>
+              </Grid>
+
+              <GridItemNosotros />
+
+
+
+            </Grid>
+          </Container>
+        </Paper>
+        <Paper id="contacto" elevation={3} style={{ padding: '20px', marginTop: '20px', boxShadow: 'none' }}>
+          <Container maxWidth="md" style={{ padding: '20px' }}>
+            <Typography variant="h4" color="primary" style={{ fontWeight: 'bold', color: '#1976d2', textAlign: 'center' }}>Contacto</Typography>
+            {/* Cuadrícula para dividir en dos partes */}
+            <Typography variant="h5" color="primary" style={{ fontWeight: 'bold' }}>Comunicate</Typography>
+            <Formulario></Formulario>
+          </Container>
         </Paper>
         <Box component="footer" style={{ backgroundColor: '#333', color: 'white', padding: '20px', textAlign: 'center', marginTop: '20px' }}>
           <Typography>Contacto: info@tusoftwarefactory.com</Typography>
